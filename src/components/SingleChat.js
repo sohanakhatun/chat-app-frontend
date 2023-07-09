db949d2e235181c14699df2305c7004e08aeea26
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2023-07-09 19:20:42 +0530

    Update SingleChat.js

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -57,7 +57,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       setLoading(true);
 
       const { data } = await axios.get(
-        `/api/message/${selectedChat._id}`,
+        `/message/${selectedChat._id}`,
         config
       );
       setMessages(data);
@@ -88,7 +88,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         };
         setNewMessage("");
         const { data } = await axios.post(
-          "/api/message",
+          "/message",
           {
             content: newMessage,
             chatId: selectedChat,
