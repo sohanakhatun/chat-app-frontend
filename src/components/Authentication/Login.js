d3d2a17fbfa9352913c52a815f84af95df4b0435
Sohana Khatun <83324509+sohanakhatun@users.noreply.github.com>
2023-07-09 19:52:50 +0530

    Update Login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -49,7 +49,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "api/user/login",
+        "/user/login",
         { email, password },
         config
       );
